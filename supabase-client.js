eee7baa306764e50de0ce9e800f35dad8317236b
Taufiq Aziz <128366806+azizt91@users.noreply.github.com>
2025-12-12 10:07:57 +0700

    Update Supabase client version in import statement

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.46.1/+esm';
 
 const supabaseUrl = 'https://bvmzgarccplvnuhbbeif.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bXpnYXJjY3Bsdm51aGJiZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1OTQ2MDEsImV4cCI6MjA3NDE3MDYwMX0.RzwmuBKuzMUXzKBc_89wPpyURUnqH6ippgXHmjNQ6yo';
